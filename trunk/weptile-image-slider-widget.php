80b6672e7c76a8a4016194190ac166e2fc26b154
weptile <weptile@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-23 15:35:59 +0000

    git-svn-id: https://plugins.svn.wordpress.org/weptile-image-slider-widget@616340 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/weptile-image-slider-widget.php
+++ trunk/weptile-image-slider-widget.php
@@ -6,6 +6,23 @@ Description: Easy, lightweight, responsive sidebar image slider widget. Utilizes
 Version: 1.0
 Author: Weptile (Algün & Ufuk)
 Author URI: http://weptile.com
+License: GPL v3
+
+Weptile Image Slider Widget
+Copyright (C) 2012, Ufuk Erdogmus - pm@weptile.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 class Weptile_Image_Slider_Widget extends WP_Widget {
